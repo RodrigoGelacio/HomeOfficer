e672f7b39eab673528784c85e1087cb1be345e8b
cmm23 <56312720+cmm23@users.noreply.github.com>
2020-05-16 14:40:49 -0500

    Clase Persona
    Agregar la clase persona para controlar a estos personajes

---
+++ src/com/brackeen/javagamebook/tilegame/sprites/Person.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.brackeen.javagamebook.tilegame.sprites;
+
+import com.brackeen.javagamebook.graphics.Animation;
+
+/**
+ *
+ * The persons to save
+ */
+public class Person extends Creature{
+    
+    public Person(Animation left, Animation right, Animation up, Animation down,
+            Animation deadLeft, Animation deadRight) {
+        super(left, right, up, down, deadLeft, deadRight);
+    }
+    
+    
+//    public void setY(float y) {
+//        super.setY(y);
+//    }
+    
+    public float getMaxSpeed() {
+        return 0.5f;
+    }
+//    
+//    public void setVelocityY(){
+//        
+//    }
+    
+}
