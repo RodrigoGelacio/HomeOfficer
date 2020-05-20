2f06d91cebad0e99a0ef631b369bd19065c187c7
cmm23 <A00825117@itesm.mx>
2020-05-20 14:54:52 -0500

    Viruses

---
+++ src/com/brackeen/javagamebook/tilegame/sprites/Virus.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.brackeen.javagamebook.tilegame.sprites;
+
+import com.brackeen.javagamebook.graphics.Animation;
+
+/**
+ *
+ * @author carol
+ */
+public class Virus extends Creature2{
+    
+    Player player;
+    
+    public Virus(Animation left, Animation right, Animation up, Animation down, Animation deadLeft, Animation deadRight) {
+        super(left, right, up, down, deadLeft, deadRight);
+    }
+    
+    public float getMaxSpeed() {
+        return 0.3f;
+    }
+  
+    //To follow the player thorughout the map
+    public void setVelocityX(){
+        if (player.getX() > getX()){
+            setX(getX() + 1);
+        }
+        else if(player.getX() < getX()){
+            setX(getX() - 1);
+        }
+    }
+    
+    public void setVelocityY(){
+        if (player.getY() > getY()){
+            setY(getY() + 1);
+        }
+        else if (player.getY() < getY()){
+            setY(getY() - 1);
+        }
+    }
+}
